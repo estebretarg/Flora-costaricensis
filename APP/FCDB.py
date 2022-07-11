9ac244420e823aca70f1154483723f5d1500e732
estebretarg <estebretarg@gmail.com>
2022-07-10 19:23:27 -0600

    DB testing

--- APP/FCDB.py
+++ APP/FCDB.py
@@ -1,13 +1,39 @@
 import sqlite3
+import numpy as np
 
 conn = sqlite3.connect('FloraCostaricensisDB.db')
 
 # Create a cursor
 cursor = conn.cursor()
 
-# create a table
-cursor.execute(
-    "INSERT INTO especies VALUES('Pinaceae', 'Pinus', 'Pinus caribea')")
+#Insert many inputs
+especies_multiples = [
+                        ('Meliaceae', 'Cedrela', 'Cedrela odorata'), 
+                        ('Combretaceae', 'Terminalia', 'Terminalia amazonia')
+                     ]
+
+#create a table
+#cursor.execute(
+    #"""CREATE TABLE *name*(
+    #   column 1 DATATYPE,
+    #   column 2 DATATYPE
+    # )""")
+
+#Insert one input
+#cursor.execute("INSERT INTO especies VALUES('x', 'y', 'z')")
+
+#Insert many inputs
+#cursor.executemany("INSERT INTO especies VALUES (?,?,?)", especies_multiples)
+
+#Query the database
+cursor.execute("SELECT rowid, * FROM especies")
+
+lista = cursor.fetchall()
+
+for sp in lista:
+    print(sp)
+
+#print(cursor.fetchall())
 
 # Commit our command
 conn.commit()
